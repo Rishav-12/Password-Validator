92bece55adaef606dcf73d1b88ee474c74d93a44
Rishav <rishavmitra10@gmail.com>
2021-02-28 23:11:55 +0530

    design changes

--- validator.java
+++ validator.java
@@ -1,9 +1,8 @@
 /*
- * A simple password validator which validates passwords based on some simple rules
- * Your password should be atleast 8 characters long and contain
- * atleast one Uppercase letter, one Lowercase letter, one Number and one Special Character
+ * A simple password validator which validates passwords based on some simple rules.
+ * Your password should be atleast 8 characters long and should contain atleast
+ * one Uppercase letter, one Lowercase letter, one Number and one Special Character.
  * This was my first tryst with GUIs in Java.
- * So it was a bit of a struggle but I enjoyed the experience.
  */
 import javax.swing.*;
 import java.awt.*;
@@ -14,13 +13,13 @@ public class validator implements ActionListener
     private static JLabel passwordLabel;
     private static JPasswordField passwordText;
     private static JButton button;
-    private static JLabel success;
+    private static JLabel info;
     public static void main(String args[])
     {
         JPanel panel = new JPanel();
 
         JFrame frame = new JFrame();
-        frame.setSize(800, 250);
+        frame.setSize(600, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Password Validator");
         frame.add(panel);
@@ -28,28 +27,33 @@ public class validator implements ActionListener
         panel.setLayout(null);
 
         passwordLabel = new JLabel("Password");
-        passwordLabel.setBounds(300, 20, 80, 25);
+        passwordLabel.setBounds(160, 20, 80, 25);
         panel.add(passwordLabel);
 
         passwordText = new JPasswordField(20);
-        passwordText.setBounds(400, 20, 165, 25);
+        passwordText.setBounds(260, 20, 165, 25);
         panel.add(passwordText);
 
         button = new JButton("Check");
-        button.setBounds(360, 60, 80, 25);
+        button.setBounds(250, 60, 80, 25);
         button.addActionListener(new validator());
         panel.add(button);
 
-        success = new JLabel("<html>Your password should be atleast 8 characters long<br/>and contain atleast one Uppercase letter, one Lowercase letter, one Number and one Special Character</html>");
-        success.setBounds(50, 120, 800, 35);
-        panel.add(success);
+        info = new JLabel("<html>Your password should be atleast 8 characters long<br/>and contain atleast one Uppercase letter,<br/>one Lowercase letter, one Number and one Special Character</html>");
+        info.setBounds(150, 120, 500, 55);
+        panel.add(info);
 
         frame.setVisible(true);
     }
 
-    public boolean checkPassword(String password)
+    public boolean checkValidPassword(String password)
     {
         int u = 0, l = 0, d = 0, sp = 0;
+        //This function returns different values based on what is missing in the parameter 'password'
+        if(password.length() < 8)
+        {
+            return false;
+        }
         for(int i = 0; i < password.length(); i++)
         {
             char c = password.charAt(i);
@@ -74,27 +78,21 @@ public class validator implements ActionListener
         {
             return true;
         }
-        else
-        {
-            return false;
-        }
+        return false;
     }
 
     public void actionPerformed(ActionEvent e)
     {
-        int flag = 0;
+        String message = "";
         String password = passwordText.getText();
-        if(password.length() >= 8)
+        if(checkValidPassword(password))
         {
-            flag = 1;
-        }
-        if(checkPassword(password) && flag == 1)
-        {
-            success.setText("The password " + password + " is strong");
+            message = "The password " + password + " is strong enough and valid.";
         }
         else
         {
-            success.setText("The password " + password + " is not strong enough");
+            message = "The password " + password + " is not strong enough.";
         }
+        info.setText(message);
     }
 }
